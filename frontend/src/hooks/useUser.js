5dac27c4a9a7b2a1ffb4159abaafb8ca874ab7f1
nalbarracin <nalbarracin@mural.co>
2022-05-02 02:42:33 -0300

    login y logout guardando token de sesion

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -1,44 +1,34 @@
-import {useCallback, useContext, useState} from 'react'
-import Context from '../context/UserContext'
+import {useCallback, useState} from 'react'
 import AuthService from '../service/AuthService'
 
 export default function useUser () {
-    const {jwt, setJWT} = useContext(Context)
     const [state, setState] = useState({ loading: false, error: false })
 
     const login = useCallback((username, password) => {
-      console.log('username');
-      console.log(username);
-      console.log('password');
-      console.log(password);
       setState({loading: true, error: false })
       AuthService.loginService(username, password)
       .then((response) => {
-        console.log(response.data);
         if (response.data.token) {
           localStorage.setItem("tokenData", JSON.stringify(response.data));
-          window.sessionStorage.setItem('jwt', jwt)
           setState({loading: false, error: false })
-          setJWT(response.data)
         }
       })
       .catch((error) => {
-        console.log(error)
         //TODO ponemos una toast aca?
-        window.sessionStorage.removeItem('jwt')
         //console.log(error.response.data.message);
         localStorage.removeItem('tokenData')
         setState({loading: false, error: true })
       });
-    }, [setJWT])
+    })
       
-      const logout = useCallback(() => {
-        window.sessionStorage.removeItem('jwt')
-        setJWT(null)
-      }, [setJWT])
+      const logout = () => {
+        console.log('logout')
+        localStorage.removeItem('tokenData')
+        setState({loading: false, error: false })
+      }
     
       return {
-        isLogged: Boolean(jwt),
+        isLogged: Boolean(localStorage.getItem('tokenData')),
         isLoginLoading: state.loading,
         hasLoginError: state.error,
         login,
