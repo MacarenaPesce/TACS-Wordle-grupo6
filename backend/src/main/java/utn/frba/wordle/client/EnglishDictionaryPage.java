f66592877e9e5581b304f780b064640063fbee48
nalbarracin <nalbarracin@mural.co>
2022-05-06 01:35:11 -0300

    Cambia la conexion hacia la api rest de diccionario para que use apacheClient

--- backend/src/main/java/utn/frba/wordle/client/EnglishDictionaryPage.java
+++ backend/src/main/java/utn/frba/wordle/client/EnglishDictionaryPage.java
@@ -3,6 +3,12 @@ package utn.frba.wordle.client;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.SneakyThrows;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -21,29 +27,27 @@ public class EnglishDictionaryPage {
     public List<String>  getDefinitions(String word ) {
         List<String> definitions = new ArrayList<>();
         String destinationUrl = "https://api.dictionaryapi.dev/api/v2/entries/EN/" + word;
-        //peticionHttpGet(String destinationUrl)
-        // Esto es lo que vamos a devolver
-        StringBuilder result = new StringBuilder();
-        // Crear un objeto de tipo URL
-        URL url = new URL(destinationUrl);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String result = "";
 
-        // Abrir la conexión e indicar que será de tipo GET
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        // Búferes para leer
-        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        // Mientras el BufferedReader se pueda leer, agregar contenido a resultado
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+        try (httpClient) {
+            HttpGet request = new HttpGet(destinationUrl);
+
+            request.addHeader("Accept", "application/json");
+            request.addHeader("Content-type", "application/json");
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    result = EntityUtils.toString(entity);
+                }
+            }
         }
 
         Gson gson = new Gson();
-
         Type userListType = new TypeToken<ArrayList<EnglishDictionaryResponse>>(){}.getType();
-
-        ArrayList<EnglishDictionaryResponse> responses = gson.fromJson(result.toString(), userListType);
-
+        ArrayList<EnglishDictionaryResponse> responses = gson.fromJson(result, userListType);
         AtomicReference<String> partialDefinition = new AtomicReference<>("");
         responses.stream()
                 .flatMap(response -> response.meanings.stream())
@@ -52,8 +56,6 @@ public class EnglishDictionaryPage {
                                 return meaning.definitions.stream();
                 })
                 .forEach(definition -> definitions.add(partialDefinition + definition.definition));
-
-        rd.close();
 
         return definitions;
     }
