e35cb7d529a888166251b5aa17ed4b32dd955712
Nico-0 <nitraceros@gmail.com>
2022-05-22 21:52:32 -0300

    boton de ayuda torneos

--- frontend/src/pages/tourney/AddMember.js
+++ frontend/src/pages/tourney/AddMember.js
@@ -2,6 +2,7 @@ import {useAddMember} from '../../hooks/useAddMember';
 import FormAddMember from './FormAddMember';
 import ModalAddMember from './ModalAddMember';
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import React from 'react'
 
 
 function AddMember (tourneyId)  {
@@ -9,12 +10,12 @@ function AddMember (tourneyId)  {
     const [isOpenModal,openModal,closeModal] = useAddMember(false);
 
 	return(
-		<div>	
+		<React.Fragment>
 			<button type="submit" className="btn btn-warning" onClick={openModal}> <AiOutlineUsergroupAdd/> </button>
 			<ModalAddMember isOpen={isOpenModal} closeModal={closeModal}>
 				<FormAddMember/>
 			</ModalAddMember>
-		</div>
+		</React.Fragment>
 	);
 }
 export default AddMember;
