70b162883a0d559c6f589157f11f584d96d6b0ee
Nico-0 <nitraceros@gmail.com>
2022-05-27 02:30:12 -0300

    ayuda en otro archivo

---
+++ frontend/src/pages/tourney/Ayuda.js
@@ -0,0 +1,92 @@
+import {AiOutlineCrown, AiOutlineUserAdd, AiOutlineUsergroupAdd} from "react-icons/ai";
+import {BsInfoLg, BsTrashFill} from "react-icons/bs";
+import {GoCheck} from "react-icons/go";
+import React from "react";
+
+
+const ayudaMis = <div className="alert alert-primary" role="alert">
+    <p>
+        <h4><p>Te salen tus torneos creados:</p></h4>
+        <button className="btn btn-warning" type="submit" >
+            <AiOutlineUsergroupAdd/>
+        </button> Boton para sumar personas
+        <p></p>
+        <button className="btn btn-primary" type="submit" href="/info">
+            <BsInfoLg/>
+        </button> Ranking del torneo
+        <p></p>
+        <button className="btn btn-danger" type="submit">
+            <BsTrashFill/>
+        </button> Finalizar torneo ya (extra)
+        <p></p>
+
+        <h4><p>Te salen torneos de otros a los que estas anotado:</p></h4>
+        <button className="btn btn-primary" type="submit" href="/info">
+            <BsInfoLg/>
+        </button> Ranking del torneo
+        <p></p>
+        <button className="btn btn-danger" type="submit">
+            <BsTrashFill/>
+        </button> Salir del torneo (extra) (dejas de estar anotado)
+        <p></p>
+        <button className="btn btn-info" type="submit">
+            <AiOutlineUserAdd/>
+        </button> Este botón se está mostrando ahora pero no va
+    </p>
+</div>
+
+const ayudaPublic =     <div className="alert alert-primary" role="alert">
+    <p>
+        <h4><p>Te salen torneos publicos de otros a los que te podes anotar:</p></h4>
+        <button className="btn btn-info" type="submit">
+            <AiOutlineUserAdd/>
+        </button> Boton para sumarte (solo si aún no estás participando)
+        <p></p>
+        <button className="btn btn-success" type="submit">
+            <GoCheck/>
+        </button> Indicador si ya te sumaste
+        <p></p>
+        <button className="btn btn-primary" type="submit" href="/info">
+            <BsInfoLg/>
+        </button> Ranking del torneo
+        <p></p>
+        <button className="btn btn-danger" type="submit">
+            <BsTrashFill/>
+        </button> Salir del torneo (extra) (solo si estas participando)
+        <p></p>
+
+        <h4><p>Te salen tus torneos publicos creados:</p></h4>
+        <button className="btn btn-warning" type="submit" >
+            <AiOutlineUsergroupAdd/>
+        </button> Boton para sumar personas
+        <p></p>
+        <button className="btn btn-primary" type="submit" href="/info">
+            <BsInfoLg/>
+        </button> Ranking del torneo
+        <p></p>
+        <button className="btn btn-danger" type="submit">
+            <BsTrashFill/>
+        </button> Finalizar torneo ya (extra)
+
+    </p>
+</div>
+
+const ayudaFin = <div className="alert alert-primary" role="alert">
+    <p>
+        <h4><p>Torneos finalizados de los cuales hayas participado/jugado:</p></h4>
+        <button className="btn btn-primary" type="submit" href="/info">
+            <BsInfoLg/>
+        </button> Ranking final del torneo (no difiere de todos los botones de ranking)
+        <p></p>
+        <button className="btn btn-warning" type="submit" href="/info">
+            <AiOutlineCrown/>
+        </button> Mostrar el ganador en una columna con este icono a su lado
+    </p>
+</div>
+
+
+export default {
+    ayudaMis,
+    ayudaPublic,
+    ayudaFin
+};
