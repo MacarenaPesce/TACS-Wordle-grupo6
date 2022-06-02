940791c7c7fd909efedc2c2236c298be2e5cf897
Nico-0 <nitraceros@gmail.com>
2022-06-02 03:51:13 -0300

    telegram recibir update

--- backend/src/main/java/utn/frba/wordle/controller/TelegramController.java
+++ backend/src/main/java/utn/frba/wordle/controller/TelegramController.java
@@ -1,6 +1,8 @@
 package utn.frba.wordle.controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,6 +13,7 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import utn.frba.wordle.model.tele.Update;
 
 import java.io.IOException;
 import java.net.URI;
@@ -19,30 +22,32 @@ import java.net.URISyntaxException;
 @RestController
 @RequestMapping("/api/telegram")
 @CrossOrigin
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class TelegramController {
+    //usar esta dependencia en el caso de necesitar todos los objetos ya modelados:
+    //https://github.com/pengrad/java-telegram-bot-api/tree/master/library/src/main/java/com/pengrad/telegrambot/model
 
-    String API_URL = "https://api.telegram.org/bot<token>/";
-    String CHAT_ID = "<chat id>";
+    String TOKEN = "";
+    String API_URL = "https://api.telegram.org/bot"+TOKEN+"/";
 
 
     @PostMapping("/")
-    public ResponseEntity<String> postUpdates(@RequestBody String mensaje) throws IOException, URISyntaxException {
+    public ResponseEntity<String> postUpdates(@RequestBody Update update) throws IOException, URISyntaxException {
 
-        System.out.println(mensaje);
+        String myString = new GsonBuilder().setPrettyPrinting().create().toJson(update);
+        System.out.println("Update recibido: \n"+myString);
 
-        enviarMensaje(mensaje);
+        enviarMensaje(myString, update.getMessage().getChat().getId());
 
-        return new ResponseEntity<>(mensaje, HttpStatus.OK);
+        return new ResponseEntity<>(myString, HttpStatus.OK);
     }
 
-    private void enviarMensaje(String mensaje) throws IOException, URISyntaxException {
+    private void enviarMensaje(String mensaje, Long chat_id) throws IOException, URISyntaxException {
         String destinationUrl = API_URL + "sendMessage";
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         HttpGet request = new HttpGet(destinationUrl);
         URI uri = new URIBuilder(request.getURI())
-                .addParameter("chat_id", CHAT_ID)
+                .addParameter("chat_id", chat_id.toString())
                 .addParameter("text", mensaje)
                 .build();
         request.setURI(uri);
