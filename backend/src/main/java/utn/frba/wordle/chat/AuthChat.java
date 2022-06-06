715342a1ce37c02c9baa0f75e986503b8fa8ec96
Nico-0 <nitraceros@gmail.com>
2022-06-06 00:24:14 -0300

    capas y definiciones de telegram

---
+++ backend/src/main/java/utn/frba/wordle/chat/AuthChat.java
@@ -0,0 +1,11 @@
+package utn.frba.wordle.chat;
+
+public class AuthChat {
+    //no existe login, el userid de telegram me asegura que solo una persona puede acceder
+    //el usuario debe registrarse la primera vez eligiendo nombre de usuario, y este se guarda en la base de datos
+
+    //al momento del primer registro agregar forma de reutilizar un usuario que ya existia en el frontend
+    //agregar forma de devolver o restaurar mi contraseña, para poder usar el mismo usuario en el frontend
+
+
+}
