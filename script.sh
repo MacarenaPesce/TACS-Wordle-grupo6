3c608b03fd8c3b42d5524f30b268f2e037142791
nalbarracin <nalbarracin@mural.co>
2022-06-08 10:52:52 -0300

    modifica el script de inicialización de docker vinculando los 3 contaikners y una BD persistida en disco - LINUX

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+docker network create mired
+
 docker volume create mysql-db-data
-docker run --name test-mysql -p3333:3306 -it -e MYSQL_ROOT_PASSWORD=root --mount src=mysql-db-data,dst=/var/lib/mysql -d mysql:8.0
-docker run -it --link test-mysql:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE wordle_borrar;"'
+docker run --network mired --name test-mysql -p3333:3306 -it -e MYSQL_ROOT_PASSWORD=root --mount src=mysql-db-data,dst=/var/lib/mysql -d mysql:8.0
+docker run -it --link test-mysql:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE wordle;"'
 
 cd ./frontend
-docker build -t grupo6/fronted .
-docker run -d -p3000:3000 grupo6/fronted
+docker build -t grupo6/frontend .
+docker run -d --network mired -p3000:3000 grupo6/fronted
 
 cd ../backend
 docker build -t grupo6/backend .
-docker run -d -it -p8080:8080 grupo6/backend
+docker run -d --network mired -it -p8080:8080 grupo6/backend
 
