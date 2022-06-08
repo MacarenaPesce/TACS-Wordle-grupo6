b5f18e7500a007ea092e354fa0b6e18a56eb93e1
nalbarracin <nalbarracin@mural.co>
2022-06-08 10:31:34 -0300

    Ajustes en script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd ./backend
-docker build -t grupo6/backend .
-docker run --name backend -d -it -p8080:8080 grupo6/backend
+docker volume create mysql-db-data
+docker run --name test-mysql -p3333:3306 -it -e MYSQL_ROOT_PASSWORD=root --mount src=mysql-db-data,dst=/var/lib/mysql -d mysql:8.0
+docker run -it --link test-mysql:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE wordle_borrar;"'
 
-cd ../frontend
+cd ./frontend
 docker build -t grupo6/fronted .
 docker run -d -p3000:3000 grupo6/fronted
 
-docker run --name test-mysql -p3333:3306 -it -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
-docker run -it --link test-mysql:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE wordle; exit;"'
-
+cd ../backend
+docker build -t grupo6/backend .
+docker run -d -it -p8080:8080 grupo6/backend
 
