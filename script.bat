95acfd721c96350eceac1c9f7d528bc1dabb01f0
nalbarracin <nalbarracin@mural.co>
2022-06-08 12:09:48 -0300

    Copia las modificaicones del script de docker al .bat

--- script.bat
+++ script.bat
@@ -1,7 +1,17 @@
+docker stop test-mysql backend frontend
+docker rm test-mysql backend frontend
+docker network rm mired
+
+docker network create mired
+
+docker volume create mysql-db-data
+docker run --network mired --name test-mysql -p3333:3306 -e MYSQL_ROOT_PASSWORD=root --mount src=mysql-db-data,dst=/var/lib/mysql -d mysql:8.0
+docker run --network mired --rm mysql sh -c 'exec mysql -htest-mysql -P3306 -uroot -proot -e "CREATE DATABASE wordle;"'
+
 cd ./backend
 docker build -t grupo6/backend .
-docker run -d -p8080:8080 grupo6/backend
+docker run -d --network mired --name backend -it -p8080:8080 grupo6/backend
 
 cd ../frontend
-docker build -t grupo6/fronted .
-docker run -d -p3000:3000 grupo6/fronted
+docker build -t grupo6/frontend .
+docker run -d --network mired --name frontend -p3000:3000 grupo6/frontend
