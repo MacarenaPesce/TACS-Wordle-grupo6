c50b5647cd9e83d050ec6327c2657869fd889df6
rmejiadelagala-utn <rmejiadelagala-utn@users.noreply.github.com>
2022-06-10 16:28:11 -0300

    Update script..bat
    se alinea con la logica del shell de linux para levantar los 3 contenedores

--- script.bat
+++ script.bat
@@ -1,12 +1,18 @@
+rem Cosas a mejorar: 
+rem   Usar compose
+rem   EN vez de hacer el run hacer un docker exec, en lugar de levantar un nuevo container para ejecutar comandos a mysql
+rem   Parametrizar url db y url del back en springboot, se pasa con -e al docker
+
+rem   hacemos un Clean up del ambiente
 docker stop test-mysql backend frontend
 docker rm test-mysql backend frontend
 docker network rm mired
 
+rem Creamos la red mired
 docker network create mired
-
+rem Creamos el volumen
 docker volume create mysql-db-data
 docker run --network mired --name test-mysql -p3333:3306 -e MYSQL_ROOT_PASSWORD=root --mount src=mysql-db-data,dst=/var/lib/mysql -d mysql:8.0
-docker run --network mired --rm mysql sh -c 'exec mysql -htest-mysql -P3306 -uroot -proot -e "CREATE DATABASE wordle;"'
 
 cd ./backend
 docker build -t grupo6/backend .
@@ -15,3 +21,12 @@ docker run -d --network mired --name backend -it -p8080:8080 grupo6/backend
 cd ../frontend
 docker build -t grupo6/frontend .
 docker run -d --network mired --name frontend -p3000:3000 grupo6/frontend
+
+rem Este ps solo para que de unos segundos que el container levante ya que si hacemos el exec si aun no termino de levantar todo, nos da error.
+docker ps
+rem ingresamos al container con su name para crear la database
+docker exec test-mysql mysql -proot -e "CREATE DATABASE wordle;"
+
+rem Despues de levantar el container del back ejecutamos el data.sql que crea la vista ranking
+cd ../backend/src/main/resources
+docker exec -i test-mysql sh -c "exec mysql -uroot -proot" < data.sql
