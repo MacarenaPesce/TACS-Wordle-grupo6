a004167e5a536ff411234963c578b71bf1608a30
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2022-06-14 08:23:00 -0300

    Botones torneos desde 0

--- frontend/src/pages/tourney/BotonesTorneos.js
+++ frontend/src/pages/tourney/BotonesTorneos.js
@@ -6,114 +6,84 @@ import AddMember from "./AddMember"
 import UserService from "../../service/UserService";
 import TourneyService from "../../service/TourneyService";
 
-
-function BotonesTorneos(data){
+export default function BotonesTorneos(data){
+    debugger
     let tourney = data.tourney;
     let userId = localStorage.getItem("userId");
+    let myTourneysid = [];
     
     console.log("data:",data);
     console.log("id torneo actual", tourney.tourneyId);
 
-    let disableButton = false;
-    console.log("disable button 1:", disableButton);
+    let disableButtonUserAdd = false;
+    let disableButtonAddMember = false;
 
-    let myTourneysid = [];
+    const clickAgregarme = () => {
+        TourneyService.join(tourney.tourneyId);
+    }
 
     const getTourney= async () =>{
         await UserService.getMyTourneysActive()
             .then(response => {
-                console.log("data de torneos activos del user:",response.data)
                 myTourneysid = response.data.map((torneo)=>torneo.tourneyId)
-                console.log("mis torneos id:", myTourneysid);
-
-                if(myTourneysid.includes(tourney.tourneyId))
-                    disableButton= true;
-                else 
-                    disableButton= false;
-        
-                console.log("disableButton 2:", disableButton); 
-                console.log("include:", myTourneysid.includes(tourney.tourneyId))
-
+                disableButtons();
             })
             .catch(error => {
                 console.log(error)
             })
+    }
+
+    const disableButtons= () =>{
+        debugger
+        if(tourney.owner.id == userId){ //es mi torneo
+            if(tourney.type === 'PUBLIC' || tourney.type === 'PRIVATE'){ // es publico o privado
+                console.log("esta en ready?",tourney.state == 'READY');
+
+                if(tourney.state == 'READY'){  // esta por empezar
+                    /* si sos el creador agregas personas ya sea publico o privado SI NO ESTA EMPEZADO*/
+                    disableButtonAddMember = false;
+                }
+                else{
+                    // si esta empezado o finalizado se deshabilita el addMember
+                    disableButtonAddMember = true;
+                }
+            }
+        }
+        else if(tourney.owner.id != userId){ // no es mi torneo
+            if(tourney.type === 'PUBLIC'){ // es publico
+                /* te permite agregarte al torneo si el torneo es de tipo publico y vos NO sos el creador y se deshabilita si ya estas en el torneo*/
+                if(myTourneysid.includes(tourney.tourneyId))
+                    disableButtonUserAdd= true;
+                else 
+                    disableButtonUserAdd= false;
+                console.log("disableButton 2:", disableButtonUserAdd); 
+            }
+            else{ // es privado pero no es mi torneo (me agregaron)
+                /*No es mi torneo, no puedo agregar a nadie*/
+                disableButtonAddMember = true;
+            }
+        }
     }
             
     useEffect(() => {
         getTourney();
     }, []);
 
-    const clickAgregarme = () => {
-        console.log("te agregaste a un torneo". tourney)
-        TourneyService.join(tourney.tourneyId);
-    }
+    return(
+        <div>
+            <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId}>
+                <BsInfoLg/> 
+            </Button>
 
-
-    if(tourney.owner.id == userId){ //es mi torneo
-        if(tourney.type === 'PUBLIC' || tourney.type === 'PRIVATE'){ // es publico o privado
-            if(tourney.state == 'READY'){  // esta por empezar
-                return(
-                    <div> 
-                        <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId} onClick={()=>console.log("acabas de tocar el boton info")}>
-                            <BsInfoLg/> 
-                        </Button>
-                        {/* si sos el creador agregas personas ya sea publico o privado SI NO ESTA EMPEZADO*/}
-                        <AddMember tourneyId ={tourney.tourneyId} 
-                                     ownerId ={tourney.owner.id}/> 
-                    </div>
-                )
+            {disableButtonAddMember ?
+                (<p></p>):
+                (<AddMember tourneyId ={tourney.tourneyId} ownerId ={tourney.owner.id}/>)
             }
-            if(tourney.state == 'STARTED'){  // esta empezado
-                return( 
-                    <div> 
-                        <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId} onClick={()=>console.log("acabas de tocar el boton info")}>
-                            <BsInfoLg/> 
-                        </Button>
-                    </div>
-                )
-            }
-            else{ // esta finalizado
-                return(
-                    <div>
-                        <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId} onClick={()=>console.log("acabas de tocar el boton info")}>
-                            <BsInfoLg/> 
-                        </Button>
-                    </div>  )                      
-            }
-        }
-    }
-    else if(tourney.owner.id != userId){ // no es mi torneo
-        if(tourney.type === 'PUBLIC'){ // es publico
-            console.log("el disable puto",disableButton);
-            return(
-                <div>
-                    <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId} onClick={()=>console.log("acabas de tocar el boton info")}>
-                        <BsInfoLg/> 
-                    </Button>
 
-                    {/* te permite agregarte al torneo si el torneo es de tipo publico y vos NO sos el creador 
-                     y se deshabilita si ya estas en el torneo*/}
-
-                    {disableButton ?
-                            (<button className="btn btn-info" type="button" disabled><AiOutlineUserAdd/></button>) :
-                            (<button className="btn btn-info" type="button" onClick={()=>clickAgregarme()}> <AiOutlineUserAdd/> </button>)
-                    }
-                </div>
-            )
-        }
-        else{ // es privado pero no es mi torneo (me agregaron)
-            return(
-                <div>
-                    <Button className="btn btn-primary" type="button" href={'info/' + tourney.tourneyId} onClick={()=>console.log("acabas de tocar el boton info")}>
-                        <BsInfoLg/> 
-                    </Button>
-                </div>                
-            )
-        }
-    }
+            {disableButtonUserAdd ?
+                (<button className="btn btn-info" type="button" disabled><AiOutlineUserAdd/></button>) :
+                (<button className="btn btn-info" type="button" onClick={()=>clickAgregarme()}> <AiOutlineUserAdd/> </button>)
+            }
+        </div>
+    )
 }
-
-export default BotonesTorneos;
-
-
